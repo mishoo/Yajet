c9e50df8a3378c2f68d2f2f498fcacf72efa84a7
Mihai Bazon <mihai@bazon.net>
2010-05-07 21:50:59 +0200

    DOC

--- js/yajet.js
+++ js/yajet.js
@@ -1,4 +1,4 @@
-//> Copyright (c) 2010, Mihai Bazon, Dynarch.com.  All rights reserved.
+//> Copyright (c) 2010, Mihai Bazon <mihai.bazon@gmail.com>, Dynarch.com.  All rights reserved.
 //>
 //> Redistribution and use in source and binary forms, with or without
 //> modification, are permitted provided that the following conditions are met:
